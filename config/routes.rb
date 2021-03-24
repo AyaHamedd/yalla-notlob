117f771dfb612f51a1608507d3e1d81b8b551740
SarahOuf <sarah.m.ouf@gmail.com>
2021-03-24 18:21:15 +0200

    fixed routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get "/home", to: "home#index"
-  get "/orders/:id/joinedFriends", to: "orders#joined_friends"
-  get "/orders/:order_id/items/:item_id/deleteItem", to: "items#delete_item"
+  get "/orders/:id/joinedFriends", to: "orders#joined_friends", as: 'joined_friends'
+  get "/orders/:order_id/items/:item_id/deleteItem", to: "items#delete_item", as: 'delete_item'
+  get "/orders/:order_id/finish", to: "orders#finish_order", as: 'finish_order'
 
   resources :orders do
     resources :items
