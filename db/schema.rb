6159113114ace090493bbc97c5324c006508fa11
SarahOuf <sarah.m.ouf@gmail.com>
2021-03-26 16:27:58 +0200

    altered column order_type to be enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_213009) do
+ActiveRecord::Schema.define(version: 2021_03_26_142513) do
 
   create_table "friends", id: false, force: :cascade do |t|
     t.integer "friend_id"
