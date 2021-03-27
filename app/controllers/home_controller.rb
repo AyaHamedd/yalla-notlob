144e935182d80493cb4146a5b5a84e5824da8718
Aya <ayahamedabdulaziz@gmail.com>
2021-03-27 03:54:29 +0200

    Get latest activities of friends

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @orders = Order.where(user: current_user);
+    @user = User.find(current_user.id)
+    @activities = Friend.joins('INNER JOIN "orders" ON "orders"."user_id" = "friends"."friend_id"').where(users_id: current_user.id).order('"orders"."created_at" desc');
   end
 end
