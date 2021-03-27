60622dafbbca815430cbf4353eaea89605440b5d
Aya <ayahamedabdulaziz@gmail.com>
2021-03-27 04:07:23 +0200

    Modify activities query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     @orders = Order.where(user: current_user);
     @user = User.find(current_user.id)
-    @activities = Friend.joins('INNER JOIN "orders" ON "orders"."user_id" = "friends"."friend_id"').where(users_id: current_user.id).order('"orders"."created_at" desc');
+    @activities = Friend.joins('INNER JOIN "orders" ON "orders"."user_id" = "friends"."friend_id"')
+    .joins('INNER JOIN "users" ON "users"."id" = "friends"."friend_id"')
+    .select("users.full_name","friends.friend_id","orders.id","orders.restaurant_name","orders.order_type","orders.created_at")
+    .where(users_id: current_user.id)
+    .order('"orders"."created_at" desc');
   end
 end
