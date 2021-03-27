df5583efd4d3ce568eae2dca05ad9dfe2bfa7766
HebaMostafa123 <hoba.mostafa97@gmail.com>
2021-03-27 02:15:04 +0200

    facebook login first version

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -342,4 +342,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  config.omniauth :facebook, "270300574553073", "5a0c7832e95f15ae071c7be81c9e4c06"
 end
