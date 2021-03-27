7e5f97b9b5ea6b7d2f769ea1db8f99ebb7a339d9
Aya <ayahamedabdulaziz@gmail.com>
2021-03-28 00:24:16 +0200

    User can add an avatar from update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,33 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :orders
   has_many :notifications , foreign_key: :recipient_id
+
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: "150x150!").processed 
+    else
+      "/default.jpg"
+    end
+  end
+  
+  private
+  def add_default_avatar
+	  unless avatar.attached?
+		  avatar.attach(
+			  io: File.open(
+          Rails.root.join(
+				  'app','assets','images','default.jpg'
+				)
+			), 
+      filename: 'default.jpg',
+			content_type: 'image/jpg'
+		)
+		end
+	end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_203807) do
+ActiveRecord::Schema.define(version: 2021_03_27_204502) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "friends", id: false, force: :cascade do |t|
     t.integer "friend_id"
@@ -82,13 +110,31 @@ ActiveRecord::Schema.define(version: 2021_03_24_203807) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
+    t.string "provider"
+    t.string "uid"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "friends", "users", column: "users_id"
   add_foreign_key "groups", "users"
   add_foreign_key "items", "orders"
